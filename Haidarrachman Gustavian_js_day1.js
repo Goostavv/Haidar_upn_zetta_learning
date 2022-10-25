fbed0476ac310ba28dead00681a302c87a265fbe
Goostavv <89502939+Goostavv@users.noreply.github.com>
2022-10-25 21:06:29 +0700

    js_day2

--- Haidarrachman Gustavian_js_day1.js
+++ Haidarrachman Gustavian_js_day1.js
@@ -2,24 +2,50 @@
 *@param {Book} book
 *@param {Number} discount 
 *@param {Number} tax
+*@param {Number} stock
+*@param {Number} order
 */
-function priceCalculator (book, discount, tax) { 
-    let result = 0; 
-    if (book && book.price) {
-const price = book.price; 
-const totalDiscount = (price* discount/100); 
-const priceAfterDiscount = price - totalDiscount; 
-const totalTax  = priceAfterDiscount * (tax/100); 
-const priceAfterTax = priceAfterDiscount + totalTax;
+const book = [
+    { name: "a", price: 139000, printing_status: true, stock: 9, purchased: 5 },
+    { name: "b", price: 174000, printing_status: true, stock: 15, purchased: 5 },
+    { name: "c", price: 90000, printing_status: true, stock: 7, purchased: 0 }
+]
+let stat = ""
+let tax = 5
+let discount =15
 
-console.group(); 
-console.log('Original Price', price); 
-console.log('Discount Amount', totalDiscount); 
-console.log('Price After Discount', priceAfterDiscount);
-console.log('Tax Amount', totalTax); 
-console.log('Price After Tax', priceAfterTax); 
-console.groupEnd;
+function purchaseBook(name,purchasing) {
+    for (let i = 0; i < book.length; i++) {
+        if (book[i].name === name && book[i].printing_status != false) {
+            if (book[i].stock == 0) {
+                stat = "stok buku habis"
+                break
+            } else {
+                const totalDiscount = book[i].price * (discount / 100)
+                const priceAfterDiscount = book[i].price - totalDiscount
+                const totalTax = priceAfterDiscount * (tax / 100)
+                const priceAfterTax = priceAfterDiscount - totalTax
+                console.group()
+                console.log("Original Price ", book[i].price)
+                console.log("Discount Amount ", totalDiscount)
+                console.log("Price after discount ", priceAfterDiscount)
+                console.log("tax amount ", totalTax)
+                console.log("Price after tax ", priceAfterTax)
+                console.log("Stock ", book[i].stock)
+                if(book[i].stock >= purchasing) {
+                    console.log("Book Available")
+                } else {
+                    console.log("out of stock")
+                }
+                console.groupEnd()
+                break
+            }
+        } else {
+            stat = "sorry your order is not available pls try again"
+        }
     }
-return result;
+    console.log(stat)
 }
-priceCalculator({title: 'ini buku', price:100000, printing_status: true}, 5, 10);
+
+
+purchaseBook("a",1)
