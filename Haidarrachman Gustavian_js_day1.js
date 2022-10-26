a07a858cc35ff8270aaaa9df2450762be383c612
Goostavv <89502939+Goostavv@users.noreply.github.com>
2022-10-26 13:35:21 +0700

    js_day3

--- Haidarrachman Gustavian_js_day1.js
+++ Haidarrachman Gustavian_js_day1.js
@@ -1,51 +1,50 @@
-/**
-*@param {Book} book
-*@param {Number} discount 
-*@param {Number} tax
-*@param {Number} stock
-*@param {Number} order
-*/
 const book = [
-    { name: "a", price: 139000, printing_status: true, stock: 9, purchased: 5 },
-    { name: "b", price: 174000, printing_status: true, stock: 15, purchased: 5 },
-    { name: "c", price: 90000, printing_status: true, stock: 7, purchased: 0 }
+    { tittlename: "a", price: 139000, printing_status: true, stock: 9, purchased: 5 },
+    { tittlename: "b", price: 174000, printing_status: true, stock: 15, purchased: 5 },
+    { tittlename: "c", price: 90000, printing_status: true, stock: 7, purchased: 0 }
 ]
-let stat = ""
-let tax = 5
-let discount =15
 
-function purchaseBook(name,purchasing) {
-    for (let i = 0; i < book.length; i++) {
-        if (book[i].name === name && book[i].printing_status != false) {
-            if (book[i].stock == 0) {
-                stat = "stok buku habis"
-                break
-            } else {
-                const totalDiscount = book[i].price * (discount / 100)
-                const priceAfterDiscount = book[i].price - totalDiscount
-                const totalTax = priceAfterDiscount * (tax / 100)
-                const priceAfterTax = priceAfterDiscount - totalTax
-                console.group()
-                console.log("Original Price ", book[i].price)
-                console.log("Discount Amount ", totalDiscount)
-                console.log("Price after discount ", priceAfterDiscount)
-                console.log("tax amount ", totalTax)
-                console.log("Price after tax ", priceAfterTax)
-                console.log("Stock ", book[i].stock)
-                if(book[i].stock >= purchasing) {
-                    console.log("Book Available")
-                } else {
-                    console.log("out of stock")
-                }
-                console.groupEnd()
-                break
-            }
+
+
+let status = ""
+let tax = 10
+let discount = 20
+let interest = 3
+let creditDue = 30
+let pricePayPerMonth = 0
+function purchaseBook(booktittle, purchase) {
+    const result = book.find(({ tittlename }) => tittlename === booktittle)
+    if (result != undefined) {
+        const totalDiscount = result.price * (discount / 100)
+        const priceAfterDiscount = result.price - totalDiscount
+        const totalTax = priceAfterDiscount * (tax / 100)
+        const priceAfterTax = priceAfterDiscount - totalTax
+        const totalBuy = priceAfterTax * purchase
+        Credit(totalBuy)
+        console.group()
+        console.log("Stock ", result.stock)
+        if (result.stock >= purchase) {
+            console.log("Book can be purchased with the requested stock")
+            console.log()
+            console.log("Original Price ", result.price)
+            console.log("Discount Amount ", totalDiscount)
+            console.log("Price after discount ", priceAfterDiscount)
+            console.log("tax amount ", totalTax)
+            console.log("Price after tax ", priceAfterTax)
+            console.log("Total Price to pay ", totalBuy)
+            console.log()
+            console.log("Price to pay with credit due every month ", pricePayPerMonth)
+            console.groupEnd()
         } else {
-            stat = "sorry your order is not available pls try again"
+            console.log("Book cannot purchased with the amount of stock")
+
         }
     }
-    console.log(stat)
 }
 
+function Credit(totalBuy){
+    pricePayPerMonth = totalBuy + (totalBuy * (interest/100))
+    return pricePayPerMonth
+}
 
-purchaseBook("a",1)
+purchaseBook("b", 5)
