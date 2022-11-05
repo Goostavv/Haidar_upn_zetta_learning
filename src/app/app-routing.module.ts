faf251f23a1aa10778dc60953aa5fcf090876c2e
Goostavv <123200075@student.upnyk.ac.id>
2022-11-05 12:52:35 +0700

    Angular day 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
+import { ListsComponent } from './lists/lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'cards/all',
+    component:CardsComponent
+  },
+  {
+    path: 'lists/all',
+    component:ListsComponent
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'lists/all'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
