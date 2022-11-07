d0615b7a2858e4b16f0c1cea58b4d7e0ad58e8c9
Goostavv <123200075@student.upnyk.ac.id>
2022-11-07 19:50:50 +0700

    angular day 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CardListRoutingModule } from './card-list/card-list-routing.module';
 import { AppComponent } from './app.component';
+import { CardsModule } from './cards/cards.module';
+import { ListsModule } from './lists/lists.module';
+import { CardListModule } from './card-list/card-list.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-  
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CardListRoutingModule,
+    CardsModule,
+    ListsModule,
+    CardListModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
