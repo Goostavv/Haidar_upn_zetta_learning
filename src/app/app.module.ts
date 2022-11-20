94e029185338c01ce2373b93ab5ef87229a8cf7f
Goostavv <123200075@student.upnyk.ac.id>
2022-11-20 11:22:59 +0700

    angular_material_day1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersRoutingModule } from './users/users-routing.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { PostsRoutingModule } from './posts/posts-routing.module';
+import { MovieLibraryModule } from './movie-library/movie-library.module';
+import { MovieLibraryRoutingModule } from './movie-library/movie-library-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    UsersRoutingModule,
+    UsersModule,
+    PostsModule,
+    PostsRoutingModule,
+    MovieLibraryModule,
+    MovieLibraryRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
